400b6452a4e986782b01ba72ca166b60902ecbfd
Devin Biddy <xp.gram@gmail.com>
2025-11-13 01:11:24 -0800

    add todo

--- scripts/game_objects/grid_entity.gd
+++ scripts/game_objects/grid_entity.gd
@@ -5,3 +5,7 @@ extends Node2D
 # TODO I see this script being inherited by movable players, enemies, interactible chests,
 #   pushable blocks, etc. This is the GameObject class, essentially.
 #   I'm not sure what I want from it yet, though.
+#
+#   Oh, I need some standard layer that allows enemies to be attacked, npcs to be talked
+#   to, and most importantly, that they register themselves on the grid and care about
+#   grid collisions and whatnot.
