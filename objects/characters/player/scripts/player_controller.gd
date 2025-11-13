d6b3167d515276e225beceeaf90621aad0085cc1
Devin Biddy <xp.gram@gmail.com>
2025-11-12 23:16:22 -0800

    add new direction sprites to player controller

--- objects/characters/player/scripts/player_controller.gd
+++ objects/characters/player/scripts/player_controller.gd
@@ -38,8 +38,21 @@ func _physics_process(delta: float) -> void:
   if vector_inputs_pressed.any(func (pressed): return pressed):
     _move_self(input);
 
+    # FIXME I hate this:
+    if vector_inputs_pressed[0]:
+      _set_facing('up');
+    elif vector_inputs_pressed[1]:
+      _set_facing('down');
+    elif vector_inputs_pressed[2]:
+      _set_facing('left');
+    elif vector_inputs_pressed[3]:
+      _set_facing('right');
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
+  # FIXME Note: because of the first if, I think, this function actually does nothing.
+
   if not event is InputEventAction:
     return;
 
@@ -48,14 +61,34 @@ func _unhandled_input(event: InputEvent) -> void:
   match action_event.action:
     'move_up':
       _move_self(Vector2(0, -1));
+      _set_facing('up');
     'move_down':
       _move_self(Vector2(0, 1));
+      _set_facing('down');
     'move_left':
       _move_self(Vector2(-1, 0));
+      _set_facing('left');
     'move_right':
       _move_self(Vector2(1, 0));
+      _set_facing('right');
 
 
 func _move_self(vector: Vector2) -> void:
   # TODO Check collisions first.
   position += vector * GRID_SIZE;
+
+
+func _set_facing(dir: StringName) -> void:
+  match dir:
+    'up':
+      animated_sprite.play('idle_up');
+      animated_sprite.scale.x = 1;
+    'down':
+      animated_sprite.play('idle_down');
+      animated_sprite.scale.x = 1;
+    'left':
+      animated_sprite.play('idle_side');
+      animated_sprite.scale.x = -1;
+    'right':
+      animated_sprite.play('idle_side');
+      animated_sprite.scale.x = 1;
