1ef6b1f319ec14b7228a3c709a082928a30f9533
Devin Biddy <xp.gram@gmail.com>
2025-11-13 01:06:39 -0800

    add unused GameObject class for grid things

---
+++ scripts/game_objects/grid_entity.gd
@@ -0,0 +1,7 @@
+class_name GridEntity
+extends Node2D
+# TODO Can enemies, which are moved by script, be CharacterBody2D's? I think so, right?
+
+# TODO I see this script being inherited by movable players, enemies, interactible chests,
+#   pushable blocks, etc. This is the GameObject class, essentially.
+#   I'm not sure what I want from it yet, though.
