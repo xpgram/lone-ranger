539b577896e5490517993b5d96b79716ab567cea
Devin Biddy <xp.gram@gmail.com>
2025-11-18 15:24:08 -0800

    add rudimentary submenu navigation; add submenus can remember their last selection indices

--- objects/system/command_menu/command_menu.gd
+++ objects/system/command_menu/command_menu.gd
@@ -1,5 +1,55 @@
 extends Node2D
 
 
+@export var ui_container: Control;
+@export var lists: Array[ItemList];
+
+var active_index := 0;
+
+
+func _ready() -> void:
+  ui_container.hide();
+
+
 func _process(_can_actdelta: float) -> void:
-  pass
+  
+  if Input.is_action_just_pressed('open_action_menu'):
+    if not ui_container.visible:
+      ui_container.show();
+      _open_submenu(0);
+    else:
+      active_index = 0;
+      ui_container.hide();
+  
+  elif Input.is_action_just_pressed('interact') and _is_open():
+    if active_index == 0:
+      active_index = _get_current_submenu_selection_index() + 1;
+      active_index = clamp(active_index, 0, lists.size());
+      _open_submenu(active_index);
+    else:
+      active_index = 0;
+      ui_container.hide();
+
+  elif Input.is_action_just_pressed('cancel'):
+    if active_index != 0:
+      active_index = 0;
+      _open_submenu(active_index);
+    else:
+      ui_container.hide();
+
+
+func _is_open() -> bool:
+  return ui_container.visible;
+
+
+func _open_submenu(index: int) -> void:
+  for list in lists:
+    list.close();
+  
+  var open_list := lists[index];
+  open_list.open();
+
+
+func _get_current_submenu_selection_index() -> int:
+  var submenu := lists[active_index];
+  return submenu.get_selected_items()[0];
--- objects/system/command_menu/item_list.gd
+++ objects/system/command_menu/item_list.gd
@@ -5,27 +5,31 @@ extends ItemList
 ## closing it.
 @export var remember_cursor_position := false;
 
-@onready var menu_cursor := %MenuCursor;
+## The last-selected item index when the menu was last seen.
+var remembered_index := 0;
+
+@onready var menu_cursor_sprite := %MenuCursor;
 
 
 func _ready() -> void:
   _disable_tooltips_for_all_items();
   item_selected.connect(_move_cursor_to_item);
 
-  # Default to the first option.
-  # FIXME Doesn't work. _ready() happens too early, I presume.
-  _self_select_item(0);
-
 
 func open() -> void:
+  print('open menu');
   show();
-  if not remember_cursor_position:
+  grab_focus();
+
+  if remember_cursor_position:
+    _self_select_item(remembered_index);
+  else:
     _self_select_item(0);
 
 
 func close() -> void:
+  remembered_index = _get_current_selection_index();
   hide();
-  # TODO Do I need to save an index number here to remember cursor position?
 
 
 func _disable_tooltips_for_all_items() -> void:
@@ -33,10 +37,28 @@ func _disable_tooltips_for_all_items() -> void:
     set_item_tooltip_enabled(i, false);
 
 
+func _get_current_selection_index() -> int:
+  var selected_items := get_selected_items();
+  return (
+    0 if selected_items.size() == 0
+    else selected_items[0]
+  );
+
+
 func _self_select_item(index: int) -> void:
+  # FIXME Remove. Should report 2, 23.0.
+  select(2);
+  item_selected.emit(index);
+
   select(index);
   item_selected.emit(index);
 
 
 func _move_cursor_to_item(index: int) -> void:
-  menu_cursor.position.y = get_item_rect(index).get_center().y;
+  var nums := [
+    index,
+    get_item_rect(index).get_center().y,
+  ];
+  print('item:%s rect.center.y = %s' % nums);
+
+  menu_cursor_sprite.position.y = get_item_rect(index).get_center().y;
