97f5d128d29e272bfd9601ff4efbc75ee3cb9f44
Devin Biddy <xp.gram@gmail.com>
2025-11-19 16:41:29 -0800

    update chest opening animation

--- objects/interactives/chest/context_action.gd
+++ objects/interactives/chest/context_action.gd
@@ -18,19 +18,17 @@ func perform_interaction_async(actor: GridEntity) -> void:
   # TODO contents is a struct type, including... I guess I'm not sure.
   # TODO initiator.has_node('Inventory')
   # TODO initiator.inventory.add(contents.item * contents.number)
+  # FIXME Okay, this is bad file structure.
+  #   I'm making changes to the way animations are called, and I forgot where this script
+  #   even was for a good second. Having to mine through every game object I've ever made
+  #   to find a ContextAction that might touch something I'm updating is misery.
+  #   I don't *necessarily* mind that these ContextActions are part of the Chest .tscn,
+  #   I kind of dread the thought of the alternative, honestly, but the ContextAction
+  #   script can't be located here. It needs to be held with all others.
 
-  var dirs := [
-    Vector2i.RIGHT,
-    Vector2i.UP,
-    Vector2i.LEFT,
-    Vector2i.DOWN,
-  ];
+  if actor is Player2D:
+    actor.facing_direction = Vector2i.DOWN;
+    actor.animation_set_player.play('item_get!', actor.facing_direction);
+    await get_tree().create_timer(1.0).timeout;
 
-  actor.facing_direction = Vector2i.DOWN;
-  await get_tree().create_timer(0.25).timeout;
-
-  for dir in dirs:
-    actor.facing_direction = dir;
-    await get_tree().create_timer(0.05).timeout;
-
-  await get_tree().create_timer(0.25).timeout;
+    actor.animation_set_player.play('idle', actor.facing_direction);
