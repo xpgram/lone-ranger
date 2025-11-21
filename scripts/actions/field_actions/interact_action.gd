3570ef4c614bd5dc829bd53c3361d5884ceadd13
Devin Biddy <xp.gram@gmail.com>
2025-11-21 10:36:05 -0800

    add thoughts and todos

--- scripts/actions/field_actions/interact_action.gd
+++ scripts/actions/field_actions/interact_action.gd
@@ -2,6 +2,40 @@ class_name Interact_FieldAction
 extends FieldAction
 
 
+# TODO Implement variable action_time_costs.
+#   As far as I know, this is the only FieldAction that *needs* this functionality,
+#   though there are others that could benefit from it.
+#   The problem is that Dialogue shouldn't cost time, but opening a chest should cost a
+#   full turn.
+#
+#   [Note: Actually, ItemUse_FieldAction is another that would benefit from this.
+#    Using a 'Potion' should definitely be a full-time cost, but unlocking a door with a
+#    key maybe shouldn't.]
+#
+#   An alternative could be dividing context actions into InteractTimeAction and
+#   InteractFreeAction that check their ContextAction nodes for which kind it is,
+#   but that could be kind of annoying.
+#
+#   Another alternative is keeping a private _action_time_cost that is updated first-thing
+#   by perform_async using a number it finds in the ContextAction, and this would be
+#   returned by action_time_cost() when it's called later.
+#   This creates a call-order dependency on these two functions, though, that I really
+#   don't like.
+#
+#   An alternative-alternative to the last idea is to keep a get_variable_time_cost() that
+#   explicitly declares its purpose and usage, I guess.
+#   ...
+#   *sigh*, I don't really hate this idea...
+#
+#   How would the TurnManager know which time_cost() function to call, though?
+#   Or rather, why wouldn't it just always call the variable one?
+#
+#   Um... We could set a flag, or we could define an inheritance type. TurnManager would
+#   know by checking first which action it's "speaking" to.
+#   Final question: is this over-engineered? There also isn't a reason not to call
+#   variable_time_cost every time.
+
+
 func action_name() -> String:
   return "Interact";
 
