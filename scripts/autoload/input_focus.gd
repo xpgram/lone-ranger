ad7e4d7712439334eedc1c047cfecd11d44dec82
Devin Biddy <xp.gram@gmail.com>
2025-11-25 12:17:29 -0800

    add input_focus global script

---
+++ scripts/autoload/input_focus.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+## Returns true if the given node is, or is an ancestor of, the currently focused control
+## node.
+func has_branch_focus(node: Node) -> bool:
+  var focus_owner := get_viewport().gui_get_focus_owner();
+
+  if not focus_owner:
+    return false;
+  elif node is Control and (node as Control).has_focus():
+    return true;
+
+  return focus_owner.is_ancestor_of(node);
