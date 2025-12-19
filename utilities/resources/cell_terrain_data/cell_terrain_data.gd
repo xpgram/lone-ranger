789699871a44e036ac53a2055ef808b97bd4bf07
xpgram <xp.gram@gmail.com>
2025-12-19 01:25:16 -0800

    add resource CellTerrainData to capture custom tile data in a type-safe way

---
+++ utilities/resources/cell_terrain_data/cell_terrain_data.gd
@@ -0,0 +1,21 @@
+## Encapsulates data for terrain objects in a tilemap.
+class_name CellTerrainData
+extends Resource
+
+
+## The world-geometry type for terrain objects.
+enum GeometryType {
+  ## Entities can fall into this type.
+  Hole,
+  ## Entities can traverse this type.
+  Floor,
+  ## Entities cannot traverse this type.
+  Wall,
+};
+
+
+## The world-geometry type for this terrain object. [br]
+## [enum GeometryType.Hole] can be fallen into. [br]
+## [enum GeometryType.Floor] can be traversed. [br]
+## [enum GeometryType.Wall] cannot be traversed. [br]
+@export var geometry_type := GeometryType.Hole;
