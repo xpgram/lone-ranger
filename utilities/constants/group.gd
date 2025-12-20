7973c9296a629ec30c60fcee646b31f3efc84bbf
xpgram <xp.gram@gmail.com>
2025-12-20 09:59:25 -0800

    add new global groups for different grid entities

--- utilities/constants/group.gd
+++ utilities/constants/group.gd
@@ -4,3 +4,15 @@ class_name Group
 
 ## A group containing TileMapLayer data for the world geometry.
 const TerrainData: StringName = 'terrain_data';
+
+## A group containing player avatars.
+const Player: StringName = 'player_entities';
+
+## A group containing enemy entities.
+const Enemy: StringName = 'enemy_entities';
+
+## A group containing non-player character entities.
+const NPC: StringName = 'npc_entities';
+
+## A group containing object entities, like chests, warp zones, etc.
+const Interactible: StringName = 'interactible_entities';
