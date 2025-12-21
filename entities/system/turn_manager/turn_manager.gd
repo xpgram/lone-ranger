12385c6d38aef5fc7346be51e672cd066536b4e3
xpgram <xp.gram@gmail.com>
2025-12-20 16:39:07 -0800

    refactor TurnManager such that the turn performance logic for enemies are extended to all entity types, even as they are performed in batches; fix GridEntity attribute ticker discrepancies

--- entities/system/turn_manager/turn_manager.gd
+++ entities/system/turn_manager/turn_manager.gd
@@ -63,9 +63,9 @@ func _advance_time(player_schedule: FieldActionSchedule) -> void:
 
   # Other turn actions
   if new_time_remaining <= 0:
-    await _perform_npc_actions_async();
-    await _perform_enemy_actions_async();
-    await _perform_object_actions_async();
+    await _perform_group_entity_actions_async(Group.NPC);
+    await _perform_group_entity_actions_async(Group.Enemy);
+    await _perform_group_entity_actions_async(Group.Interactible);
 
   # Reset for next turn
   var next_time_remaining := new_time_remaining if new_time_remaining > 0 else PartialTime.FULL;
@@ -77,71 +77,45 @@ func _advance_time(player_schedule: FieldActionSchedule) -> void:
   turn_in_progress = false;
 
 
-## Trigger NPC turn actions.
-func _perform_npc_actions_async() -> void:
-  var npc_entities: Array[GridEntity];
-  npc_entities.assign(get_tree().get_nodes_in_group(Group.NPC));
-
-  var any_npc_acted := false;
-
-  for npc in npc_entities:
-    if npc.has_method('can_act') and npc.can_act():
-      # TODO Use multipass method to make sure all actors do something?
-      await npc.act_async();
-      npc.update_attributes();
-      any_npc_acted = true;
-
-  if any_npc_acted:
-    await _perform_wait_async();
-
-
-## Trigger enemy turn actions.
-func _perform_enemy_actions_async() -> void:
+## For all [GridEntity]'s in group [param entity_group], request and await their turn
+## actions and after-effects on the game board.
+func _perform_group_entity_actions_async(entity_group: StringName) -> void:
   var include_golems := golem_time >= PartialTime.FULL;
 
-  var enemy_entities: Array[Enemy2D];
-  enemy_entities.assign(
+  var actor_components: Array[BoardActorComponent];
+  actor_components.assign(
     get_tree()
-      .get_nodes_in_group(Group.Enemy)
-      .filter(func (enemy: Enemy2D): return include_golems or not enemy.observes_golem_time)
+      .get_nodes_in_group(entity_group)
+      .filter(func (entity: GridEntity):
+        return (
+          Component.has_component(entity, BoardActorComponent)
+          and (include_golems or not entity.observes_golem_time)
+          # TODO observes_golem_time only makes sense to BoardActor's, so should probably be located there.
+        ))
+      .map(func (entity: GridEntity): return Component.get_component(entity, BoardActorComponent))
   );
 
-  for enemy in enemy_entities:
-    enemy.prepare_to_act();
+  if actor_components.size() == 0:
+    return;
 
-  # This multipass approach allows all enemies to act independently of their list order,
-  # avoiding scenarios where one enemy obsructs the action of another.
+  for actor in actor_components:
+    actor.prepare_to_act();
+
+  # This multipass approach allows all entities in a turn-group to act independently of
+  # their list order, avoiding scenarios where one entity obstructs the action of another.
   for i in range(3):
-    var enemy_promises: Array = enemy_entities \
-      .filter(func (enemy: Enemy2D): return not enemy.has_acted()) \
-      .map(func (enemy: Enemy2D): return enemy.act_async);
+    var actor_promises: Array = actor_components \
+      .filter(func (actor: BoardActorComponent): return not actor.has_acted()) \
+      .map(func (actor: BoardActorComponent): return actor.act_async);
 
-    await Promise.all(enemy_promises).finished;
+    await Promise.all(actor_promises).finished;
 
-  for enemy in enemy_entities:
-    enemy.update_attributes();
+  for actor in actor_components:
+    actor.get_entity().update_attributes();
 
   # FIXME This obviously shouldn't go here.
   if player.current_animation_state == 'injured':
     await get_tree().create_timer(0.5).timeout;
-
-
-## Trigger passive, interactive object "actions".
-func _perform_object_actions_async() -> void:
-  var interactive_entities: Array[Interactive2D];
-  interactive_entities.assign(get_tree().get_nodes_in_group(Group.Interactible));
-
-  var any_interactive_acted := false;
-
-  for interactive in interactive_entities:
-    if interactive.has_method('can_act') and interactive.can_act():
-      # TODO Use multipass method to make sure all actors do something?
-      await interactive.act_async();
-      interactive.update_attributes();
-      any_interactive_acted = true;
-
-  if any_interactive_acted:
-    await _perform_wait_async();
 
 
 ## Trigger a short time break.
