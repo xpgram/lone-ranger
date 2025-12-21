a89a6cd7e7a4c84abd8d5ee4183921e5d14b1d31
xpgram <xp.gram@gmail.com>
2025-12-20 19:33:20 -0800

    update doc strings for Component methods

--- common/component/component.gd
+++ common/component/component.gd
@@ -5,8 +5,8 @@ class_name Component
 ## Returns true if the given [param node] has a registered component of type
 ## [param component_type]. [br]
 ##
-## [param component_type] should be a class reference to any type that extends
-## [BaseComponent].
+## [param component_type] should be an object instance or a class reference to any type
+## that extends [BaseComponent].
 static func has_component(node: Node, component_type: Variant) -> bool:
   if not node:
     return false;
@@ -15,7 +15,9 @@ static func has_component(node: Node, component_type: Variant) -> bool:
 
 
 ## Registers the given [param component] to [param node] using the component's class name
-## as the metadata key.
+## as the metadata key. [br]
+##
+## [param component] should be an object instance of any type that extends [BaseComponent].
 static func set_component(node: Node, component: BaseComponent) -> void:
   if not node:
     return;
@@ -25,8 +27,8 @@ static func set_component(node: Node, component: BaseComponent) -> void:
 
 ## Returns the [param component_type] registered to [param node], or null. [br]
 ##
-## [param component_type] should be a class reference to any type that extends
-## [BaseComponent].
+## [param component_type] should be an object instance or a class reference to any type
+## that extends [BaseComponent].
 static func get_component(node: Node, component_type: Variant) -> BaseComponent:
   if not node:
     push_error("Cannot get a component from a null value.");
@@ -39,8 +41,8 @@ static func get_component(node: Node, component_type: Variant) -> BaseComponent:
 ## Removes the [param component_type] registered to [param node], or if no component
 ## exists, does nothing. [br]
 ##
-## [param component_type] should be a class reference to any type that extends
-## [BaseComponent].
+## [param component_type] should be an object instance or a class reference to any type
+## that extends [BaseComponent].
 static func remove_component(node: Node, component_type: Variant) -> void:
   if not node:
     return;
