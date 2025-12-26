590059f51563d2e76359b4ca20093d1aed80232c
xpgram <xp.gram@gmail.com>
2025-12-26 09:16:12 -0800

    remove unused signal from events.gd

--- utilities/autoload/events.gd
+++ utilities/autoload/events.gd
@@ -2,7 +2,5 @@ extends Node
 
 @warning_ignore_start('UNUSED_SIGNAL')
 
-## Notifies listeners that a node now exists.
-## This is useful, for instance, to allow nodes of some type to be listened for by
-## NodeContainer layers, who will add such nodes to their own list of children.
-signal node_created(entity: Node);
+
+signal some_example_signal();
