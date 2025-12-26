191820bceff33ad93e70ffebfdeace13c34514eb
xpgram <xp.gram@gmail.com>
2025-12-26 09:13:56 -0800

    remove Node extension from constants.gd

--- utilities/constants/constants.gd
+++ utilities/constants/constants.gd
@@ -1,6 +1,6 @@
 ## A utility script containing information common to the program.
 class_name Constants
-extends Node
+
 
 ## The size of a tile, or an inhabitable world space, in pixels.
 const GRID_SIZE := 16;
