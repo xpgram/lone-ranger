79bb88438a5d97c64222f1fc63abd02175a53b7a
xpgram <xp.gram@gmail.com>
2025-12-26 10:03:47 -0800

    add an enum type to PartialTime and a conversion method to get real seconds from an enum value

--- utilities/constants/partial_time.gd
+++ utilities/constants/partial_time.gd
@@ -1,5 +1,5 @@
 ## @static [br]
-## An enum for common Partial Time values.
+## A collection of common Partial Time constants.
 ##
 ## Partial Time describes fractions of turn-"seconds" measured in real-time seconds.
 ## All PartialTime values are measured in real-time, but are named according to their
@@ -18,3 +18,34 @@ const MOST      := TURN_ELAPSE_LENGTH * 0.75;
 const HALF      := TURN_ELAPSE_LENGTH * 0.5;
 const QUARTER   := TURN_ELAPSE_LENGTH * 0.25;
 const NONE      := TURN_ELAPSE_LENGTH * 0.0;
+
+
+## An enum for common [PartialTime] lengths. [br]
+##
+## This type exists for developer-UI purposes, such as the inspector, and should otherwise
+## be unnecessary. [br]
+##
+## To get the equivalent real-time seconds for each enum value, either reference them
+## directly (e.g. [member PartialTime.HALF]), or use [method PartialTime.enum_to_seconds].
+enum TimeSegment {
+  NONE,
+  QUARTER,
+  HALF,
+  MOST,
+  FULL,
+};
+
+
+## A dictionary that maps [TimeSegment] values to their real-time second equivalents.
+static var _conversion_dict: Dictionary[TimeSegment, float] = {
+  TimeSegment.NONE: NONE,
+  TimeSegment.QUARTER: QUARTER,
+  TimeSegment.HALF: HALF,
+  TimeSegment.MOST: MOST,
+  TimeSegment.FULL: FULL,
+};
+
+
+## Returns the number of real-time seconds that are equivalent to [param time_segment].
+static func enum_to_seconds(time_segment: TimeSegment) -> float:
+  return _conversion_dict[time_segment];
