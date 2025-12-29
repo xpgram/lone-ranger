aaa004e00c8153f2013d396d917338f6810ce450
xpgram <xp.gram@gmail.com>
2025-12-28 18:09:03 -0800

    patch silly all-enemies-die issue

--- utilities/nodes/components/grid_entity_components/health_component.gd
+++ utilities/nodes/components/grid_entity_components/health_component.gd
@@ -10,6 +10,13 @@ extends BaseComponent
 
 
 func _ready() -> void:
+  # FIXME All enemies share the same IntMeter resource, so they all die together, lmao.
+  #  This might be a good case for IntMeter being a Node. It just seemed annoying to implement that.
+  #  IntMeter's simple purpose has grown pretty extravagant anyway, so eh.
+  #  It's either that, or we let HealthComponent extend IntMeter extend BaseComponent, which...
+  #  I guess would be fine? It would require the fewest changes.
+  meter = meter.duplicate();
+
   if Engine.is_editor_hint():
     return;
   
