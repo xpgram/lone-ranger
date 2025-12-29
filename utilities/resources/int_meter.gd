b9bc2ddb75a7745a33091ed0134e7169c97d4868
xpgram <xp.gram@gmail.com>
2025-12-28 17:53:18 -0800

    add previous values to value-changed signals emitted from IntMeter

--- utilities/resources/int_meter.gd
+++ utilities/resources/int_meter.gd
@@ -14,15 +14,15 @@ signal empty();
 
 ## Emitted whenever the meter's [member value] is set to a new value. Is **not** emitted
 ## when [member value] is set to the same value.
-signal value_changed(value: int);
+signal value_changed(value: int, old_value: int);
 
 ## Emitted whenever the meter's [member minimum] is set to a new value. It **not** emitted
 ## when [member minimum] is set to the same value.
-signal minimum_changed(minimum: int);
+signal minimum_changed(minimum: int, old_minimum: int);
 
-## Emitted whenever the meter's [member maxnimum] is set to a new value. It **not**
+## Emitted whenever the meter's [member maximum] is set to a new value. It **not**
 ## emitted when [member maximum] is set to the same value.
-signal maximum_changed(minimum: int);
+signal maximum_changed(maximum: int, old_maximum: int);
 
 
 ## Whether the meter should emit its event signals in the engine editor context.
@@ -35,7 +35,7 @@ signal maximum_changed(minimum: int);
     value = clampi(number, minimum, maximum);
 
     if _can_emit_signals() and old_value != value:
-      value_changed.emit(value);
+      value_changed.emit(value, old_value);
 
       if value == minimum:
         empty.emit();
@@ -54,7 +54,7 @@ signal maximum_changed(minimum: int);
       value = minimum;
 
     if _can_emit_signals() and old_minimum != minimum:
-      minimum_changed.emit(minimum);
+      minimum_changed.emit(minimum, old_minimum);
 
 ## The maximum limit for the meter.
 @export var maximum: int = 1:
@@ -68,7 +68,7 @@ signal maximum_changed(minimum: int);
       value = maximum;
 
     if _can_emit_signals() and old_maximum != maximum:
-      maximum_changed.emit(maximum);
+      maximum_changed.emit(maximum, old_maximum);
 
 ## Sets the meter's value to its maximum.
 @export_tool_button('Set Full') var set_meter_to_full = _set_meter_to_full;
