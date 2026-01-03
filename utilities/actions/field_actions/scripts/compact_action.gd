380ff60a9b1de0d055722d64b9844ef759833180
xpgram <xp.gram@gmail.com>
2026-01-03 13:33:28 -0800

    create compact action script and tres

--- utilities/actions/field_actions/scripts/compact_action.gd
+++ utilities/actions/field_actions/scripts/compact_action.gd
@@ -3,8 +3,22 @@ extends FieldAction
 
 
 func can_perform(playbill: FieldActionPlaybill) -> bool:
-  return false;
+  var entities := Grid.get_entities(playbill.target_position);
+  return entities \
+    .any(func (entity: GridEntity): return entity.has_attribute('compactible_to_floor'));
 
 
 func perform_async(playbill: FieldActionPlaybill) -> void:
-  pass
+  var entities := Grid.get_entities(playbill.target_position);
+  
+  for entity in entities:
+    if entity.has_attribute('compactible_to_floor'):
+      entity.queue_free();
+
+  var tile_layers: Array[GridTileMapLayer];
+  tile_layers.assign(Engine.get_main_loop().get_nodes_in_group(Group.TerrainData));
+
+  var changeset := BetterTerrain.create_terrain_changeset(tile_layers[0], {
+    playbill.target_position: 2,
+  });
+  BetterTerrain.apply_terrain_changeset(changeset);
