57b1ead4682a0c63fa043700d6eedc0ca2da7fc6
xpgram <xp.gram@gmail.com>
2026-01-03 12:29:55 -0800

    create unimplemented compact spell script

---
+++ utilities/actions/field_actions/scripts/compact_action.gd
@@ -0,0 +1,10 @@
+class_name Compact_FieldAction
+extends FieldAction
+
+
+func can_perform(playbill: FieldActionPlaybill) -> bool:
+  return false;
+
+
+func perform_async(playbill: FieldActionPlaybill) -> void:
+  pass
