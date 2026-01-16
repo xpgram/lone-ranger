30611f544aca42e0da24060b1faf4c254185d941
xpgram <xp.gram@gmail.com>
2026-01-16 13:05:19 -0800

    add unused Stimulus for suddenly-grounded entities

--- entities/grid_entities/stimulus.gd
+++ entities/grid_entities/stimulus.gd
@@ -5,5 +5,11 @@ class_name Stimulus
 ## When a [GridEntity] is knocked into by a kinetic force, but not enough to move it.
 const bumped := &'bumped';
 
+## When a [GridEntity] is positioned over a floor space. [br]
+##
+## This signal is specifically useful when a floor tile is created beneath an already
+## falling entity.
+const is_over_ground := &'is_over_ground';
+
 ## When a [GridEntity] is positioned over a floorless space.
 const is_over_pit := &'is_over_pit';
