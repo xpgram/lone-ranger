cf9105540483d7b9ae1f52c0acfeba30d15c63d8
xpgram <xp.gram@gmail.com>
2026-01-16 12:45:33 -0800

    add grow spell script

---
+++ utilities/actions/field_actions/scripts/grow_action.gd
@@ -0,0 +1,19 @@
+class_name Grow_FieldAction
+extends FieldAction
+
+
+func can_perform(playbill: FieldActionPlaybill) -> bool:
+  var entities := Grid.get_entities(playbill.target_position);
+  return entities \
+    .any(func (entity: GridEntity): return entity is GrowableTwigEntity);
+
+
+func perform_async(playbill: FieldActionPlaybill) -> bool:
+  var entities := Grid.get_entities(playbill.target_position);
+  
+  for entity in entities:
+    if entity is GrowableTwigEntity:
+      await entity.activate_growth_async();
+      entity.queue_free();
+
+  return true;
