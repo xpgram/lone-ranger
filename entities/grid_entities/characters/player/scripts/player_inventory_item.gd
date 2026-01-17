568d069feb5bdc1368e8592431902054c1c4203e
xpgram <xp.gram@gmail.com>
2026-01-17 08:32:41 -0800

    change inventory item property

--- entities/grid_entities/characters/player/scripts/player_inventory_item.gd
+++ entities/grid_entities/characters/player/scripts/player_inventory_item.gd
@@ -6,5 +6,5 @@ extends Resource
 ## The action this inventory item represents in the inventory.
 @export var action: FieldAction;
 
-## Counts the number of times this action may still be used.
-@export var uses_remaining := 1;
+## How many copies of this item that are owned.
+@export var quantity := 1;
