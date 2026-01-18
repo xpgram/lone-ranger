7674c052c9814654b238d41257740336b92f55d3
xpgram <xp.gram@gmail.com>
2026-01-18 11:27:24 -0800

    add pickup spawn field to diggable_spot

--- entities/grid_entities/interactives/diggable_spot/diggable_spot.gd
+++ entities/grid_entities/interactives/diggable_spot/diggable_spot.gd
@@ -2,6 +2,18 @@ class_name DiggableSpotEntity
 extends GridEntity
 
 
+@export var _pickup_spawn: PackedScene;
+
+
 func _on_spell_activated__raise() -> void:
-  # TODO Create an item pickup.
+  _spawn_pickup();
   queue_free();
+
+
+func _spawn_pickup() -> void:
+  if not _pickup_spawn:
+    return;
+
+  var pickup_entity: GridEntity = _pickup_spawn.instantiate();
+  add_sibling(pickup_entity);
+  pickup_entity.grid_position = grid_position;
