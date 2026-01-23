16742a863dab3b0dfde120ec33258e7f12ba928b
xpgram <xp.gram@gmail.com>
2026-01-22 20:35:36 -0800

    add 'saving' animation to angel statue use

--- entities/grid_entities/interactives/angel_statue/context_action.gd
+++ entities/grid_entities/interactives/angel_statue/context_action.gd
@@ -15,8 +15,19 @@ func perform_interaction_async(actor: GridEntity) -> void:
   if actor is not Player2D:
     return;
 
+  # FIXME I need some way of making the screen shader more accessible.
+  #  Uhh... I guess that's kinda what events are for? Hm.
+  var whiteout_phase_time := 0.333;
+  var whiteout_tween := get_tree().create_tween();
+  whiteout_tween.tween_method(actor._shader_rect.set_silhoette_white_threshhold, 1.0, 0.4, whiteout_phase_time);
+  whiteout_tween.tween_interval(whiteout_phase_time);
+  whiteout_tween.tween_method(actor._shader_rect.set_silhoette_white_threshhold, 0.4, 1.0, whiteout_phase_time);
+  whiteout_tween.play();
+
   var health_component := Component.get_component(actor, HealthComponent) as HealthComponent;
   health_component.set_hp_to_full();
 
   # FIXME scuffed private variable access :p
   actor._starting_position = save_idol.grid_position + Vector2i.DOWN;
+
+  await whiteout_tween.finished;
