5662968687c0df7e0bca19236eb5acf40431f0e4
xpgram <xp.gram@gmail.com>
2026-01-24 03:23:03 -0800

    add functions to AudioBus

--- utilities/autoload/audio_bus.gd
+++ utilities/autoload/audio_bus.gd
@@ -6,6 +6,22 @@ func _ready() -> void:
   Events.one_shot_sound_emitted.connect(_on_one_shot_sound_emitted);
 
 
+func play_audio_scene(scene: PackedScene) -> AudioStreamPlayer:
+  var audio_instance: AudioStreamPlayer = scene.instantiate();
+  add_child(audio_instance);
+
+  if not audio_instance.playing:
+    audio_instance.play();
+
+  return audio_instance;
+
+
+func play_dynamic_audio_scene(scene: PackedScene) -> void:
+  # TODO scene is of a special multi-track type that plays one or another
+  #  audio track depending on its settings.
+  pass
+
+
 func _on_one_shot_sound_emitted(audio_scene: PackedScene) -> void:
   var audio_player: AudioStreamPlayer = audio_scene.instantiate();
   add_child(audio_player);
