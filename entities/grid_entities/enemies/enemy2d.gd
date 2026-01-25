b38be26aae1042d22fe7f72bfbd53f469eae05f1
xpgram <xp.gram@gmail.com>
2026-01-25 12:18:14 -0800

    fix enemies can still move after falling

--- entities/grid_entities/enemies/enemy2d.gd
+++ entities/grid_entities/enemies/enemy2d.gd
@@ -27,4 +27,17 @@ func _bind_health_listeners() -> void:
 ##
 ## If overriding, note that this function normally calls [code]queue_free()[/code].
 func _on_health_empty() -> void:
+  _disable_actor_component();
   queue_free();
+
+
+func _on_free_fall() -> void:
+  _disable_actor_component();
+  super._on_free_fall();
+
+
+## Tells the actor component to refuse all behavior requests.
+func _disable_actor_component() -> void:
+  var actor_component := Component.get_component(self, GridActorComponent) as GridActorComponent;
+  if actor_component:
+    actor_component.disable();
