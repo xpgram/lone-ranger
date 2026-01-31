12a5e38d6bb96c4306791b7526fd9ce5499b0343
xpgram <xp.gram@gmail.com>
2026-01-31 14:11:44 -0800

    add Sleep action to act as a reset button for player soft-locks

---
+++ utilities/actions/field_actions/scripts/rest_action.gd
@@ -0,0 +1,20 @@
+## The "I'm stuck!" action to reset the player state. [br]
+##
+## Technically, this is like casting "Sleep" on self, so it could be expanded later, maybe.
+class_name Rest_FieldAction
+extends FieldAction
+
+
+func can_perform(playbill: FieldActionPlaybill) -> bool:
+  var is_player := (playbill.performer is Player2D);
+  var is_targeting_self := (playbill.performer.grid_position == playbill.target_position);
+  return is_player and is_targeting_self;
+
+
+func perform_async(playbill: FieldActionPlaybill) -> bool:
+  var actor := playbill.performer;
+
+  if actor is Player2D:
+    actor.trigger_rest_and_reset_state();
+
+  return true;
