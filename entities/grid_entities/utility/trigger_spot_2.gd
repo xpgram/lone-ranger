37c0a719fbd9775cd2f404dae8565af2fd48f0d7
xpgram <xp.gram@gmail.com>
2026-02-05 20:43:19 -0800

    add exited signal to TriggerSpot_2

--- entities/grid_entities/utility/trigger_spot_2.gd
+++ entities/grid_entities/utility/trigger_spot_2.gd
@@ -2,13 +2,18 @@ class_name TriggerSpot_2
 extends GridEntity
 
 
-signal triggered(entity: GridEntity);
+## Emitted when a [GridEntity] collides with this one.
+signal entered(entity: GridEntity);
+
+## Emitted when a [GridEntity] separates, or stops colliding with this one.
+signal exited(entity: GridEntity);
 
 
 func _bind_stimulus_callbacks() -> void:
   super._bind_stimulus_callbacks();
   _stimulus_event_map.add_events({
     Stimulus.entity_collision: _on_collision,
+    Stimulus.entity_separation: _on_separation,
   });
 
 
@@ -19,4 +24,14 @@ func _on_collision() -> void:
     return;
 
   # FIXME Missing the entity argument.
-  triggered.emit();
+  entered.emit(player);
+
+
+func _on_separation() -> void:
+  var player := ActionUtils.get_player_entity();
+
+  if player.grid_position == grid_position:
+    return;
+
+  # FIXME Missing the entity argument.
+  exited.emit(player);
