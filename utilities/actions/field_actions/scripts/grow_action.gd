57fe6fad733fe915bb5d42890538340231e72d1d
xpgram <xp.gram@gmail.com>
2026-04-27 02:16:25 -0700

    partially transition the grow action to be more generic about its influence on the game board

--- utilities/actions/field_actions/scripts/grow_action.gd
+++ utilities/actions/field_actions/scripts/grow_action.gd
@@ -8,12 +8,16 @@ const _scene_cast_audio := preload('uid://dje6ncwpv7kxg');
 func can_perform(playbill: FieldActionPlaybill) -> bool:
   var entities := Grid.get_entities(playbill.target_position);
   return entities \
-    .any(func (entity: GridEntity): return entity is GrowableTwigEntity);
+    .any(func (entity: GridEntity): return entity.has_attribute('is_growable'));
 
 
 func perform_async(playbill: FieldActionPlaybill) -> bool:
   ActionUtils.play_cast_animation(playbill.performer, playbill.orientation);
   AudioBus.play_audio_scene(_scene_cast_audio);
+
+  # TODO This method should trigger a 'growth interact' on the object affected, and that
+  #   object should do as it will, so that we can offer different kinds of growth behavior
+  #   beyond path creation.
 
   var entities := Grid.get_entities(playbill.target_position);
   
