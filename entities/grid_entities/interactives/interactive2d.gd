8ec9e91604e2814e6d8d4a1b4f5d7b3ca7e285fc
xpgram <xp.gram@gmail.com>
2026-06-18 20:30:30 -0700

    remove interactive2d _on_free_fall override

--- entities/grid_entities/interactives/interactive2d.gd
+++ entities/grid_entities/interactives/interactive2d.gd
@@ -5,12 +5,3 @@ extends GridEntity
 
 func _init() -> void:
   add_to_group(Group.Interactible);
-
-
-## @override [br]
-## By default, interactibles do not fall into the void.
-func _on_free_fall() -> void:
-  # [FIXME] Is it better to have this be a toggleable option via GridEntity?
-  #   The problem is that overriding this method completely circumvents such a toggle.
-  #   Unless the Grid singleton does the pre-signal checking?
-  pass;
