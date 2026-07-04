68d7828719888736c9de794a71b403a1fc9466fe
xpgram <xp.gram@gmail.com>
2026-07-04 14:05:48 -0700

    add todo

--- utilities/autoload/persistence_key.gd
+++ utilities/autoload/persistence_key.gd
@@ -11,3 +11,19 @@ func get_bool(key: String) -> bool:
 
 func set_bool(key: String, value: bool) -> void:
   _bool_dictionary.set(key, value);
+
+
+
+# [TODO] Expand the persistence key system.
+#
+# class GlobalPersistenceKey
+# extends Component
+#   :Uses a key-name as-is. It's entirely up to the developer to avoid conflicts.
+#   :When interacted with, loaded, or unloaded, it automatically sets/reads its own key
+#   :and value into the PersistenceKey master dictionary.
+#
+# class LocalPersistenceKey
+# extends GlobalPersistenceKey
+#   :Fully self-contained. May use its own UID for uniqueness across the project.
+#   :May be given a debug name as well.
+#
