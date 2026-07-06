e17f670ed469c546dec3da0bac6b466f7995ad20
Dei Valko <xp.gram@gmail.com>
2026-07-06 03:29:04 -0700

    add LocalPersistenceKey auto-generates a key_uid, makes it read-only in the editor

--- utilities/nodes/components/persistence_components/local_persistence_key.gd
+++ utilities/nodes/components/persistence_components/local_persistence_key.gd
@@ -12,26 +12,23 @@ extends PersistenceKeyResource
     persistence_key = value;
     resource_name = persistence_key;
 
-## A unique identifier used to avoid naming conflicts between persistence keys. [br]
+## A unique-ish random identifier used to avoid naming conflicts between
+## persistence keys. It is recommended you still try to name your keys
+## intelligently, however. [br]
 ##
-## Note that because this is an editable field, this cannot be strictly enforced.
-## It is generally preferable to let the object set its own [member key_uid], and
-## only modify it to resolve issues.
+## In the rare case of a collision, this value may be edited in the .tscn or
+## .tres this Resource is saved in.
 @export var key_uid: StringName;
 
 
-func _property_can_revert(property: StringName) -> bool:
-  if property == 'key_uid':
-    return true;
-
-  return false;
+func _init() -> void:
+  if not key_uid:
+    key_uid = _generate_key_uid();
 
 
-func _property_get_revert(property: StringName) -> Variant:
-  if property == 'key_uid':
-    return resource_scene_unique_id as StringName;
-
-  return null;
+func _validate_property(property: Dictionary) -> void:
+  if property.name == 'key_uid':
+    property.usage |= PROPERTY_USAGE_READ_ONLY;
 
 
 func _get_key() -> StringName:
@@ -41,3 +38,8 @@ func _get_key() -> StringName:
     key += ' %s' % persistence_key;
 
   return key;
+
+
+## Returns a string with a unique-ish random set of characters.
+func _generate_key_uid() -> StringName:
+  return "Pkey_%s" % generate_scene_unique_id();
