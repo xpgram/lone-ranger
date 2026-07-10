fbcd51e5ea1d9afe6f6a616abba2d5f42dc48cde
Dei Valko <xp.gram@gmail.com>
2026-07-10 13:56:26 -0700

    update todos

--- entities/grid_entities/utility/map_snapshotter.gd
+++ entities/grid_entities/utility/map_snapshotter.gd
@@ -5,36 +5,51 @@
 #
 # Grid.set_tile_type(place, terrain_type, permanent: bool)
 #
-# This function gets a new parameter: permanent. If permanent is false, then
-# Grid also sets this change into a map undo/redo schedule that can be played
-# backwards on board resets. If permanent is true, the same change is put into
-# a permanent schedule that is bundled with the player's save data.
+# [ ] set_tile_type() logs all map changes into an undo/redo schedule.
+# [ ] The undo/redo schedule is played back on board-reset events.
+# [ ] set_tile_type(..., permanent: bool) logs map changes in a save-file schedule.
 #
-# When you push a Tail statue into a pit, the TriggerBox will have to... signal
-# something, I dunno, such that the Tail statue sets a permanent change instead
-# of a temporary one.
+# [ ] The Tail statue gets a pair-type collidable that Tail uses to discern
+#     undoable from permanent map changes.
+#     [ ] This pair-type is generalizable: it is a box-area that puts itself, or
+#         a known child maybe, into each Grid spot it covers; Tail, on collision,
+#         asks it for a message its been given, like `"tail-permanent"` or
+#         `ColliderMessageEnum.TailPermanent`, or something like that.
+#         I suppose we could also filter accepted collisions by telling it what
+#         objects it's looking for. Tail would call
+#         `collider_message.get_message(self)`, and `get_message()` would only
+#         return something useful if `self` matches its criteria.
 #
-# Maybe Tail can have a special collision-box entity that tells it to be
-# permanent. Like, the Tail statue gets pushed, the TriggerBox reads the
-# collision and sets a persistence key, then the Tail statue reads a collision
-# from this Tail-permanence-box and sets permanence mode on, other stuff
-# happens, and finally Tail is allowed to do its fall animation thing that
-# includes changing the map state, now permanently.
+# DemonWall boss, seperate feature branch:
+# [ ] These map changes will be undoable by default.
+# [ ] The DemonWall entity is resettable unless the player reached the hallway end.
 #
-# I wonder if this is generalizable, but I don't hate this idea. Tail can have
-# its own pair box, I suppose.
-#
-# I'm trying to think if this covers all my bases, and... I think it does.
-#
-# Extending the MapLayer type to allow me to draw submaps that are paintable
-# onto the main map according to persistence keys or otherwise would be really
-# useful; it's something I'll need to do eventually anyway. And, having a similar
-# tool that snapshots an area of the map as it is now to be painted later would
-# also be nice. But, I don't think... either are necessary for this board-
-# resetting Tail feature.
-#
-# It will be necessary for the Wall boss. That whole road needs to reset to
-# baseline every single time the boss is reset to 'undefeated'.
-#
-# Unless I use submaps to redesign him such that he doesn't actually modify the
-# map at all.
+# Changing map details:
+# [ ] Implement sub-TileMapLayers that can be drawn and used to paint into the
+#     main one, like a stamp.
+# [ ] A node (MapStamp?) exists to work with BooleanSpawner that, when loaded,
+#     immediately stamps its sub-tilemap. And that map may be cleared as well.
+#       - For now, this is not automatic, it just has a method call.
+#         I think I'd prefer having a seperate blank stamp in the BooleanSpawner
+#         to reset previous changes, for now.
+# [ ] MapStamp has a yellow border describing its affected area.
+# [ ] (stretch) MapSnapshot takes a 'picture' of a map area as-is and produces a
+#     MapStamp for use later.
+# [ ] MapStamp and/or BooleanSpawner:
+#     [ ] Has a Default container that is respawned whether the persistence key
+#         is true or false. The True/False containers are then loaded _after_.
+#         (This allows me to more easily create modifications to existing loads,
+#         like for TileMaps, where I actually want a messy, painted-over look
+#         for narrative reasons.)
+# The MapStamp / BooleanSpawner problem:
+#   - There are a few ways of configuring this.
+#       1. BooleanSpawner has MapStamp's in its various paint containers, and
+#          a Default-container blank stamp.
+#       2. MapStamp also has a default/true/false persistence key split, and
+#          is simply kept in the nodetree near or even inside the BooleanSpawner,
+#          just not in the spawner's own default/true/false containers.
+#   - Idea 1 ought to be easier to implement, and should be good enough.
+#   - Idea 2 may provide more opportunities for developer tools, though, like
+#     easier previewing of the true/false states? This benefit is vague enough
+#     that I think it's worth choosing idea 1 such that we can learn what it is
+#     we want.
