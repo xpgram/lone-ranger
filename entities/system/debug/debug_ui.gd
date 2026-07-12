86961894688b16a65720a96bdd6929e5c22cf667
Dei Valko <xp.gram@gmail.com>
2026-07-11 23:31:33 -0700

    adjust cmd line controls; darken cmd line placeholder text

--- entities/system/debug/debug_ui.gd
+++ entities/system/debug/debug_ui.gd
@@ -7,15 +7,14 @@ extends Control
     _show_debug_panel = value;
     _set_visible(_show_debug_panel);
 
-    if _show_debug_panel:
-      line_edit.grab_focus();
-
 
 @onready var line_edit: LineEdit = %LineEdit;
 
 
 func _ready() -> void:
   _set_visible(_show_debug_panel);
+
+  line_edit.text_submitted.connect(_on_command_line_submitted);
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -27,8 +26,21 @@ func _unhandled_input(event: InputEvent) -> void:
   #     grab_focus();
 
   if event.pressed and event.keycode == KEY_F1:
-    _show_debug_panel = !_show_debug_panel;
+    _show_debug_panel = true;
     accept_event();
+
+  if event.pressed and event.keycode == KEY_F2:
+    _show_debug_panel = true;
+    line_edit.grab_focus();
+    accept_event();
+
+  if event.pressed and event.keycode == KEY_ESCAPE:
+    if line_edit.has_focus():
+      line_edit.release_focus();
+      accept_event();
+    elif _show_debug_panel == true:
+      _show_debug_panel = false;
+      accept_event();
 
   if has_focus():
     # [FIXME] This doesn't do anything, and I think that _might_ be because
@@ -43,3 +55,9 @@ func _set_visible(visible_enabled: bool) -> void:
     hide();
   else:
     show();
+
+
+##
+func _on_command_line_submitted(text: String) -> void:
+  line_edit.text = "";
+  print(text);
