80d4a74fcd3980a712d3c8f251b6524f26f15d25
Dei Valko <xp.gram@gmail.com>
2026-07-29 14:57:02 -0700

    add export properties; add todos

--- entities/grid_entities/interactives/extendable_bridge/extendable_bridge.gd
+++ entities/grid_entities/interactives/extendable_bridge/extendable_bridge.gd
@@ -2,10 +2,22 @@ class_name ExtendableBridgeEntity
 extends Interactive2D
 
 # [TODO] Allow drawing a path for this bridge, like the GrowTwig.
+# [ ] A list of extended-to points.
+# [ ] These are drawn/shown in-editor.
+# [ ] _sprite is duplicated to each extended space.
 
+
+## The [Grid]-positions this extendable bridge occupies.
+##
+## The `self` location is always included and need not be listed here.
+@export var _multi_bridge_points := [] as Array[Vector2i];
 
 ## If true, powering this device hides the bridge instead of extending it.
 @export var _invert_power := false;
+
+## The time in seconds between each bridge piece revealing itself during the
+## power-on or power-off animation.
+@export var _bridge_reveal_step_time := 0.15;
 
 
 @onready var _sprite: Sprite2D = %Sprite2D;
